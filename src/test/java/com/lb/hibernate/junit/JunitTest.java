4faf769fa77b00bf3b40dfc8281ec2add50b9cc1
libo-pc <372647444@qq.com>
2016-10-19 08:44:24 +0800

    初始创建

---
+++ src/test/java/com/lb/hibernate/junit/JunitTest.java
@@ -0,0 +1,12 @@
+package com.lb.hibernate.junit;
+
+import org.junit.Test;
+
+public class JunitTest {
+	
+	@Test
+	public void testForSysOut(){
+		System.out.println("this is the JunitTest.testForSysOut()");
+	}
+	
+}
